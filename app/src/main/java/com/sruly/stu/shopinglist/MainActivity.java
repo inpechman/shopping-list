b54d0ff02b69b692a44b1cf467d50356a61a96f9
inpechman <inpechman@gmail.com>
2018-04-18 18:18:30 +0300

    111

--- app/src/main/java/com/sruly/stu/shopinglist/MainActivity.java
+++ app/src/main/java/com/sruly/stu/shopinglist/MainActivity.java
@@ -1,11 +1,15 @@
 package com.sruly.stu.shopinglist;
 
+import android.content.res.AssetManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.SparseIntArray;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +22,18 @@ public class MainActivity extends AppCompatActivity {
         sparseIntArray.put(123,352345);
         sparseIntArray.append(234,667);
         System.out.println("AAA " + sparseIntArray);
+        try {
+            AssetManager mgr = getAssets();
+            InputStream is = mgr.open("default_settings.json");
+            byte[] buffer = new byte[1024];
+            int counter = 0;
+            String str = "";
+            while ((counter = is.read(buffer)) != -1){
+                str += new String(buffer,0, counter);
+            }
+            System.out.println("AAA " + str);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
